176c98225b4aff436634576f01fb382f9dbc07fc
stanleylin924 <stanleylin924@gmail.com>
2020-10-10 11:46:59 +0800

    Update script to install ROS Kinetic.

--- test/install_ros_kinetic.sh
+++ test/install_ros_kinetic.sh
@@ -8,6 +8,17 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update -y
 sudo apt-get install -y ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-rqt*
+sudo apt-get install python-rosdep
+sudo rosdep init
+rosdep update
+sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+source /opt/ros/kinetic/setup.bash
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
 
 exit 0
 
